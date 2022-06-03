4030b1f414aae83425fcf7c8b344669d6390da8e
Felipe Vilvert Pilz <felipevilvertpilz@gmail.com>
2022-06-03 20:56:47 -0300

    Added Expense Class

---
+++ src/main/java/br/com/ficticiusClean/model/Expense.java
@@ -0,0 +1,17 @@
+package br.com.ficticiusClean.model;
+
+import javax.persistence.Column;
+import lombok.Data;
+
+@Data
+public class Expense
+{
+	@Column
+	private Integer gasPrice;
+
+	@Column
+	private Integer kmInCity;
+
+	@Column
+	private Integer kmInHighway;
+}
